a741d3045f2e5b9ec473c257fb9312191da2f706
Stefan677890 <szymonmisztal2626@gmail.com>
2026-03-04 19:06:15 +0100

    Version 0.8
    Finally added fixed working Perimeter Calculator

--- Perimeter Calculator/Geometric-shapes-calculator/Form1.Designer.cs
+++ Perimeter Calculator/Geometric-shapes-calculator/Form1.Designer.cs
@@ -39,13 +39,15 @@
             Bok1 = new Label();
             label1 = new Label();
             label2 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
             SuspendLayout();
             // 
             // comboBox1
             // 
             comboBox1.Font = new Font("Segoe UI", 15F);
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(443, 81);
+            comboBox1.Location = new Point(553, 94);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(180, 36);
             comboBox1.TabIndex = 0;
@@ -55,7 +57,7 @@
             // 
             Rodzajfigury.AutoSize = true;
             Rodzajfigury.Font = new Font("Segoe UI", 15F);
-            Rodzajfigury.Location = new Point(443, 50);
+            Rodzajfigury.Location = new Point(553, 50);
             Rodzajfigury.Name = "Rodzajfigury";
             Rodzajfigury.Size = new Size(123, 28);
             Rodzajfigury.TabIndex = 1;
@@ -64,25 +66,27 @@
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 15F);
-            textBox1.Location = new Point(180, 81);
+            textBox1.Location = new Point(103, 96);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(180, 34);
             textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // btn_Wynik
             // 
-            btn_Wynik.Location = new Point(443, 232);
+            btn_Wynik.Location = new Point(553, 200);
             btn_Wynik.Name = "btn_Wynik";
-            btn_Wynik.Size = new Size(180, 42);
+            btn_Wynik.Size = new Size(180, 67);
             btn_Wynik.TabIndex = 3;
             btn_Wynik.Text = "Calculate";
             btn_Wynik.UseVisualStyleBackColor = true;
+            btn_Wynik.Click += btn_Wynik_Click;
             // 
             // Obwod
             // 
             Obwod.AutoSize = true;
             Obwod.Font = new Font("Segoe UI", 25F);
-            Obwod.Location = new Point(206, 335);
+            Obwod.Location = new Point(214, 318);
             Obwod.Name = "Obwod";
             Obwod.Size = new Size(250, 46);
             Obwod.TabIndex = 4;
@@ -92,7 +96,7 @@
             // 
             Wynik.AutoSize = true;
             Wynik.Font = new Font("Segoe UI", 25F);
-            Wynik.Location = new Point(528, 335);
+            Wynik.Location = new Point(530, 318);
             Wynik.Name = "Wynik";
             Wynik.Size = new Size(38, 46);
             Wynik.TabIndex = 5;
@@ -101,7 +105,7 @@
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 15F);
-            textBox2.Location = new Point(180, 159);
+            textBox2.Location = new Point(318, 96);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(180, 34);
             textBox2.TabIndex = 6;
@@ -109,7 +113,7 @@
             // textBox3
             // 
             textBox3.Font = new Font("Segoe UI", 15F);
-            textBox3.Location = new Point(180, 240);
+            textBox3.Location = new Point(103, 233);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(180, 34);
             textBox3.TabIndex = 7;
@@ -118,7 +122,7 @@
             // 
             Bok1.AutoSize = true;
             Bok1.Font = new Font("Segoe UI", 15F);
-            Bok1.Location = new Point(180, 50);
+            Bok1.Location = new Point(103, 63);
             Bok1.Name = "Bok1";
             Bok1.Size = new Size(63, 28);
             Bok1.TabIndex = 8;
@@ -129,7 +133,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15F);
-            label1.Location = new Point(180, 128);
+            label1.Location = new Point(318, 52);
             label1.Name = "label1";
             label1.Size = new Size(63, 28);
             label1.TabIndex = 9;
@@ -139,17 +143,37 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 15F);
-            label2.Location = new Point(180, 209);
+            label2.Location = new Point(103, 192);
             label2.Name = "label2";
             label2.Size = new Size(63, 28);
             label2.TabIndex = 10;
             label2.Text = "side 3";
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 15F);
+            textBox4.Location = new Point(318, 233);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(180, 34);
+            textBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.Location = new Point(318, 192);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 28);
+            label4.TabIndex = 12;
+            label4.Text = "side 4";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(textBox4);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(Bok1);
@@ -180,5 +204,7 @@
         private Label Bok1;
         private Label label1;
         private Label label2;
+        private TextBox textBox4;
+        private Label label4;
     }
 }
